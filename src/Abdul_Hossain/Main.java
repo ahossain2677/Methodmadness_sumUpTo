f4c4046f8b066c302214a105450109ba3f0781c1
BT_1N3_08@bktech.local <BT_1N3_08@bktech.local>
2018-10-10 08:23:45 -0400

    Initial commit

---
+++ src/Abdul_Hossain/Main.java
@@ -0,0 +1,14 @@
+package Abdul_Hossain;
+
+public class sumUpTo
+{
+
+    public static void main(String[] args)
+    {
+        public static int n ();
+        {
+            for (int i = 0 ;  i < n ; i++)
+                System.out.println();
+        }
+    }
+}
